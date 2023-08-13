afe4d89ecb7a25f6bc43b20e40679a2b937b4d36
Софья Микуленко <nephilim45@yandex.ru>
2023-08-13 20:55:46 +1000

    dddddddddd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j6@xv+g_xzt*p6&0r=0041-t4n8r8xj#tn52$=$c1s5$aw^$!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['84.252.131.215', '127.0.0.1', 'localhost']
 
